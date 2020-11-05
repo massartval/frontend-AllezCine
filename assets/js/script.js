88029f93369ab84f2c7e827f1a0b9b621913b17f
massartval <massart.val@gmail.com>
2020-11-05 22:03:39 +0100

    Confirm GDPR modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,54 @@
 // On Load
 
-// Populate Jumbotron
+//   Populate Jumbotron
 
-// Populate Cards
+//   Populate Cards
 
-// Confirm cookies
+//   Confirm cookies
+// https://www.jqueryscript.net/other/GDPR-Cookie-Consent-Bootstrap-4-bsgdprcookies.html
+document.getElementById("body").bsgdprcookies();
 
 // On Click
 
-// Register form
+//   Register form
 
-// Login form
+class Account {
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+}
+// Checking "conditions" activates the register button
+document.getElementById("conditions").addEventListener("change", (event) => {
+  if (event.target.checked) {
+    document.getElementById("register").classList.add("active");
+  } else {
+    document.getElementById("register").classList.remove("active");
+  }
+});
+// Clicking "register" creates a new account or sends an alert
+document.getElementById("register").addEventListener("click", () => {
+  if (document.getElementById("password1") === document.getElementById("password2")) {
+    var newAccount = new Account(username, email, password);
+    newAccount.username = document.getElementById("username1");
+    newAccount.email = document.getElementById("email");
+    newAccount.password = document.getElementById("password1");
+  } else {
+    alert("Passwords do not match");
+  }
+});
 
-// Footer arrow
+//   Login form
+document.getElementById("login").addEventListener("click", () => {
+  if (document.getElementById("username") === newAccount.username && document.getElementById("password") === newAccount.password) {
+    alert("You are now logged in!");
+  } else {
+    alert("Please check your username and password or create an account");
+  }
+});
+
+//   Footer arrow
 // let arrowToTop = document.getElementById("footer-arrow");
 let arrowToTop;
 window.addEventListener("scroll", () => {
